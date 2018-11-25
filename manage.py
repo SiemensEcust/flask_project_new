19f51ae2952cf41a40f5d9a7f1a176f828f1810b
XR0118 <xr94118@163.com>
2018-11-25 22:11:34 +0800

    auth, user manager, main page view function and review code style

---
+++ manage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from app import creat_app, db
+from app.models import Role, User, Factory, Eqp, Supplier
+from flask_script import Manager, Shell
+from app.util.db.mongo import mongo_service
+from gevent import monkey
+monkey.patch_all()
+
+app = creat_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, Role=Role, User=User, Factory=Factory, Eqp=Eqp, Supplier=Supplier, mongo_service=mongo_service)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+
+if __name__ == '__main__':
+    manager.run()
